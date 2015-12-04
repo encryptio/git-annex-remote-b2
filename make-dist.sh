59634f500591b68949ae538fe3ecddb7addff097
Chris Kastorff <encryptio@gmail.com>
2015-12-04 03:12:23 +0000

    make-dist: added

---
+++ make-dist.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+for GOOS in darwin linux; do
+    for GOARCH in 386 amd64; do
+        export GOOS
+        export GOARCH
+
+        DIR="git-annex-remote-b2.$GOOS-$GOARCH"
+        echo "Creating ${DIR}.tar.gz"
+        rm -rf "$DIR"
+        mkdir "$DIR"
+
+        go build -o "$DIR/git-annex-remote-b2"
+        cp README.md LICENSE "$DIR/"
+
+        rm -f "$DIR".tar.gz
+        tar -czf "$DIR".tar.gz "$DIR"
+        rm -rf "$DIR"
+    done
+done
