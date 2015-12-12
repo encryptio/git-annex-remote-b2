24d73b2842de3113142aed10267f64fbd74889ff
Chris Kastorff <encryptio@gmail.com>
2015-12-12 10:33:50 -0800

    factor out external protocol details to another repo
    Fixes #4

--- main.go
+++ main.go
@@ -1,82 +1,45 @@
 package main
 
 import (
-	"bufio"
 	"bytes"
 	"crypto/sha1"
 	"encoding/hex"
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"strings"
 
+	"github.com/encryptio/go-git-annex-external/external"
 	"gopkg.in/kothar/go-backblaze.v0"
 )
 
-var (
-	in     = bufio.NewReader(os.Stdin)
-	out    = bufio.NewWriter(os.Stdout)
+type B2Ext struct {
 	bucket *backblaze.Bucket
 	prefix string
-)
-
-func getConfig(name string) string {
-	out.WriteString("GETCONFIG ")
-	out.WriteString(name)
-	out.WriteString("\n")
-	out.Flush()
-
-	line, err := in.ReadString('\n')
-	if err != nil {
-		log.Fatalf("Couldn't get config variable %s: %v", name, err)
-	}
-	line = strings.TrimSuffix(line, "\n")
-
-	if strings.HasPrefix(line, "VALUE ") {
-		return strings.TrimPrefix(line, "VALUE ")
-	}
-
-	return ""
 }
 
-func prepare(mode string) {
-	err := prepareInternal()
+func authenticate(e *external.External) (*backblaze.B2, error) {
+	accountID, err := e.GetConfig("accountid")
 	if err != nil {
-		out.WriteString(mode + "-FAILURE " + err.Error() + "\n")
-		return
+		return nil, err
 	}
-
-	out.WriteString(mode + "-SUCCESS\n")
-}
-
-func prepareInternal() error {
-	accountID := getConfig("accountid")
 	if accountID == "" {
 		accountID = os.Getenv("B2_ACCOUNT_ID")
 	}
 	if accountID == "" {
-		return errors.New("You must set accountid to the backblaze account id")
+		return nil, errors.New("You must set accountid to the backblaze account id")
 	}
 
-	appKey := getConfig("appkey")
+	appKey, err := e.GetConfig("appkey")
+	if err != nil {
+		return nil, err
+	}
 	if appKey == "" {
 		appKey = os.Getenv("B2_APP_KEY")
 	}
 	if appKey == "" {
-		return errors.New("You must set appkey to the backblaze application key")
-	}
-
-	bucketName := getConfig("bucket")
-	if bucketName == "" {
-		return errors.New("You must set bucket to the bucket name")
-	}
-
-	prefix = getConfig("prefix")
-	// prefix == "" is ok.
-	if prefix != "" && !strings.HasSuffix(prefix, "/") {
-		prefix = prefix + "/"
+		return nil, errors.New("You must set appkey to the backblaze application key")
 	}
 
 	b2, err := backblaze.NewB2(backblaze.Credentials{
@@ -84,41 +47,82 @@ func prepareInternal() error {
 		ApplicationKey: appKey,
 	})
 	if err != nil {
-		return fmt.Errorf("Couldn't authorize: %v", err)
+		return nil, fmt.Errorf("Couldn't authorize: %v", err)
 	}
 
-	bucket, err = b2.Bucket(bucketName)
+	return b2, nil
+}
+
+func getBucketConfig(e *external.External) (bucket string, prefix string, err error) {
+	bucket, err = e.GetConfig("bucket")
 	if err != nil {
-		return fmt.Errorf("Couldn't open bucket %#v: %v", bucketName, err)
+		return "", "", err
+	}
+	if bucket == "" {
+		return "", "", errors.New("You must set bucket to the bucket name")
+	}
+
+	prefix, err = e.GetConfig("prefix")
+	// prefix == "" is ok.
+	if prefix != "" && !strings.HasSuffix(prefix, "/") {
+		prefix = prefix + "/"
+	}
+
+	return bucket, prefix, nil
+}
+
+func (be *B2Ext) setup(e *external.External, canCreateBucket bool) error {
+	if be.bucket != nil {
+		// already done!
+		return nil
+	}
+
+	b2, err := authenticate(e)
+	if err != nil {
+		return err
+	}
+
+	bucketName, prefix, err := getBucketConfig(e)
+	if err != nil {
+		return err
+	}
+
+	bucket, err := b2.Bucket(bucketName)
+	if err != nil {
+		return fmt.Errorf("couldn't open bucket %#v: %v", bucketName, err)
 	}
 
 	if bucket == nil {
+		if !canCreateBucket {
+			return fmt.Errorf("bucket %#v does not exist anymore", bucketName)
+		}
+
 		fmt.Fprintf(os.Stderr, "Creating private B2 bucket %#v\n", bucketName)
+
 		bucket, err = b2.CreateBucket(bucketName, backblaze.AllPrivate)
 		if err != nil {
-			return fmt.Errorf("Couldn't create bucket %#v: %v", bucketName, err)
+			return fmt.Errorf("couldn't create bucket %#v: %v", bucketName, err)
 		}
 	}
+
+	be.bucket = bucket
+	be.prefix = prefix
 
 	return nil
 }
 
-func store(key, file string) {
-	ok := false
-	defer func() {
-		if ok {
-			out.WriteString("TRANSFER-SUCCESS STORE ")
-		} else {
-			out.WriteString("TRANSFER-FAILURE STORE ")
-		}
-		out.WriteString(key)
-		out.WriteString("\n")
-	}()
+func (be *B2Ext) InitRemote(e *external.External) error {
+	return be.setup(e, true)
+}
 
+func (be *B2Ext) Prepare(e *external.External) error {
+	return be.setup(e, false)
+}
+
+func (be *B2Ext) Store(e *external.External, key, file string) error {
 	fh, err := os.Open(file)
 	if err != nil {
-		log.Printf("Couldn't open %v for reading: %v", file, err)
-		return
+		return err
 	}
 	defer fh.Close()
 
@@ -146,186 +150,128 @@ func store(key, file string) {
 		haveSHA = sha.Sum(nil)
 	}()
 
-	res, err := bucket.ListFileNames(prefix+key, 1)
+	res, err := be.bucket.ListFileNames(be.prefix+key, 1)
 	if err != nil {
-		log.Printf("Couldn't list filenames: %v", err)
-		return
+		return fmt.Errorf("couldn't list filenames: %v", err)
 	}
 
-	if len(res.Files) > 0 && res.Files[0].Name == prefix+key {
+	if len(res.Files) > 0 && res.Files[0].Name == be.prefix+key {
 		// file probably already stored; make sure using the SHA1
-		b2file, err := bucket.GetFileInfo(res.Files[0].ID)
+		b2file, err := be.bucket.GetFileInfo(res.Files[0].ID)
 		if err != nil {
-			log.Printf("Couldn't get file info for %v: %v", res.Files[0].ID, err)
-			return
+			return fmt.Errorf("couldn't get file info for %#v: %v", res.Files[0].ID, err)
 		}
 		if b2file != nil {
 			<-shaReady
 
 			wantSHA, err := hex.DecodeString(b2file.ContentSha1)
 			if err == nil && bytes.Equal(haveSHA, wantSHA) {
-				ok = true
-				return
+				// File already exists with correct data.
+				return nil
 			}
 
 			// File exists but is the incorrect data. Delete the old version
-			// first.
-			_, err = bucket.DeleteFileVersion(prefix+key, b2file.ID)
+			// first; B2 will keep the old version around otherwise.
+			_, err = be.bucket.DeleteFileVersion(be.prefix+key, b2file.ID)
 			if err != nil {
-				log.Printf("Couldn't delete old file version: %v", err)
-				return
+				return fmt.Errorf("couldn't delete old file version: %v", err)
 			}
 		}
 	}
 
 	<-shaReady
 	if shaError != nil {
-		log.Printf("Couldn't hash local file %v: %v", file, shaError)
-		return
+		return fmt.Errorf("couldn't hash local file %v: %v", file, shaError)
 	}
 
-	_, err = bucket.UploadHashedFile(prefix+key, nil, NewProgressReader(fh, out), hex.EncodeToString(haveSHA), contentLength)
+	_, err = be.bucket.UploadHashedFile(
+		be.prefix+key,
+		nil,
+		external.NewProgressReader(fh, e),
+		hex.EncodeToString(haveSHA),
+		contentLength)
 	if err != nil {
-		log.Printf("Couldn't upload file: %v", err)
-		return
+		return fmt.Errorf("couldn't upload file: %v", err)
 	}
 
-	ok = true
+	return nil
 }
 
-func retrieve(key, file string) {
-	ok := false
-	defer func() {
-		if ok {
-			out.WriteString("TRANSFER-SUCCESS RETRIEVE ")
-		} else {
-			out.WriteString("TRANSFER-FAILURE RETRIEVE ")
-		}
-		out.WriteString(key)
-		out.WriteString("\n")
-	}()
+func (be *B2Ext) Retrieve(e *external.External, key, file string) error {
+	fh, err := os.Create(file)
+	if err != nil {
+		return fmt.Errorf("couldn't open %v for writing: %v", file, err)
+	}
+	defer fh.Close()
 
-	_, rc, err := bucket.DownloadFileByName(prefix + key)
+	_, rc, err := be.bucket.DownloadFileByName(be.prefix + key)
 	if rc != nil {
 		defer rc.Close()
 	}
 	if err != nil {
-		log.Printf("Couldn't download file: %v", err)
-		return
+		return err
 	}
 
-	fh, err := os.Create(file)
+	_, err = io.Copy(fh, external.NewProgressReader(rc, e))
 	if err != nil {
-		log.Printf("Couldn't open %v for writing: %v", file, err)
-		return
-	}
-	defer fh.Close()
-
-	_, err = io.Copy(fh, NewProgressReader(rc, out))
-	if err != nil {
-		log.Printf("Couldn't download file: %v", err)
-		return
+		return err
 	}
 
-	ok = true
+	return nil
 }
 
-func checkPresent(key string) {
-	ret := "UNKNOWN"
-	defer func() {
-		out.WriteString("CHECKPRESENT-" + ret + " " + key + "\n")
-	}()
-
-	res, err := bucket.ListFileNames(prefix+key, 1)
+func (be *B2Ext) CheckPresent(e *external.External, key string) (bool, error) {
+	res, err := be.bucket.ListFileNames(be.prefix+key, 1)
 	if err != nil {
-		log.Printf("Couldn't list filenames: %v", err)
-		return
+		return false, fmt.Errorf("couldn't list filenames: %v", err)
 	}
 
-	if len(res.Files) == 0 || res.Files[0].Name != prefix+key {
-		ret = "FAILURE"
+	if len(res.Files) == 0 || res.Files[0].Name != be.prefix+key {
+		return false, nil
 	} else {
-		ret = "SUCCESS"
+		return true, nil
 	}
 }
 
-func remove(key string) {
-	ret := "FAILURE"
-	defer func() {
-		out.WriteString("REMOVE-" + ret + " " + key + "\n")
-	}()
-
-	res, err := bucket.ListFileNames(prefix+key, 1)
+func (be *B2Ext) Remove(e *external.External, key string) error {
+	res, err := be.bucket.ListFileNames(be.prefix+key, 1)
 	if err != nil {
-		log.Printf("Couldn't list filenames: %v", err)
-		return
+		return fmt.Errorf("couldn't list filenames: %v", err)
 	}
 
-	if len(res.Files) == 0 || res.Files[0].Name != prefix+key {
-		// File already non-existent
-		ret = "SUCCESS"
-		return
+	if len(res.Files) == 0 || res.Files[0].Name != be.prefix+key {
+		// File already non-existent, nothing to remove
+		return nil
 	}
 
-	_, err = bucket.DeleteFileVersion(res.Files[0].Name, res.Files[0].ID)
+	_, err = be.bucket.DeleteFileVersion(res.Files[0].Name, res.Files[0].ID)
 	if err != nil {
-		log.Printf("Couldn't delete file version: %v", err)
-		return
+		return fmt.Errorf("couldn't delete file version: %v", err)
 	}
 
-	ret = "SUCCESS"
+	return nil
+}
+
+func (be *B2Ext) GetCost(e *external.External) (int, error) {
+	return 0, external.ErrUnsupportedRequest
+}
+
+func (be *B2Ext) GetAvailability(e *external.External) (external.Availability, error) {
+	return external.AvailabilityGlobal, nil
+}
+
+func (be *B2Ext) WhereIs(e *external.External, key string) (string, error) {
+	return "", external.ErrUnsupportedRequest
 }
 
 func main() {
-	out.Write([]byte("VERSION 1\n"))
+	h := &B2Ext{}
 
-	for {
-		out.Flush()
-
-		line, err := in.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-
-			log.Fatal(err)
-		}
-		line = strings.TrimSuffix(line, "\n")
-
-		switch {
-		case strings.HasPrefix(line, "INITREMOTE"):
-			prepare("INITREMOTE")
-
-		case strings.HasPrefix(line, "PREPARE"):
-			prepare("PREPARE")
-
-		case strings.HasPrefix(line, "TRANSFER "):
-			line = strings.TrimPrefix(line, "TRANSFER ")
-
-			fields := strings.SplitN(line, " ", 3)
-			for len(fields) < 3 {
-				fields = append(fields, "")
-			}
-
-			switch fields[0] {
-			case "STORE":
-				store(fields[1], fields[2])
-			case "RETRIEVE":
-				retrieve(fields[1], fields[2])
-			default:
-				out.WriteString("UNSUPPORTED-REQUEST\n")
-			}
-
-		case strings.HasPrefix(line, "CHECKPRESENT "):
-			key := strings.TrimPrefix(line, "CHECKPRESENT ")
-			checkPresent(key)
-
-		case strings.HasPrefix(line, "REMOVE "):
-			key := strings.TrimPrefix(line, "REMOVE ")
-			remove(key)
-
-		default:
-			out.WriteString("UNSUPPORTED-REQUEST\n")
-		}
+	err := external.RunLoop(os.Stdin, os.Stdout, h)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
 	}
+
+	os.Exit(0)
 }
